08ae24185d996c16c4d71173fd323dfc66a399bb
Thomas Mattone <thomasmattone.perso@gmail.com>
2025-01-16 17:23:14 +0100

    feat: editor toolbar button

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('PPTX extension is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pptx-viewer" is now active!');
+  // Register the command for the editor toolbar button
+  const openVirtualDocCommand = vscode.commands.registerCommand('pptx.openVirtual', async () => {
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor) {
+      vscode.window.showErrorMessage('No active editor detected.');
+      return;
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('pptx-viewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from PPTX Viewer!');
-	});
+    const fileName = activeEditor.document.fileName;
+    if (!fileName.endsWith('.pptx')) {
+      vscode.window.showErrorMessage('This command only works for .pptx files.');
+      return;
+    }
 
-	context.subscriptions.push(disposable);
+    vscode.window.showInformationMessage(`Virtual document triggered for: ${fileName}`);
+  });
+
+  context.subscriptions.push(openVirtualDocCommand);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  console.log('PPTX extension has been deactivated.');
+}
